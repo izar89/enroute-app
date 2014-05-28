c5eaf2c574f418200a31883f1d47d33de6b59e94
stijnHeylen <stijn.heylen@student.howest.be>
2014-05-28 11:13:55 +0200

    XCODE project files start

---
+++ enroute-app/enroute-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  enroute-app
+//
+//  Created by Stijn Heylen on 28/05/14.
+//  Copyright (c) 2014 Stijn Heylen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ enroute-app/enroute-app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  enroute-app
+//
+//  Created by Stijn Heylen on 28/05/14.
+//  Copyright (c) 2014 Stijn Heylen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
